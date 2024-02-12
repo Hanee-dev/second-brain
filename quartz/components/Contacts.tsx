de4b807554140303a58db05b418ab63bab571be9
Hanee-dev <hanee.devel@gmail.com>
2024-02-12 17:30:42 +0900

    Contact layout - modify information

--- quartz/components/Contacts.tsx
+++ quartz/components/Contacts.tsx
@@ -9,10 +9,12 @@ export default (() => {
 		<ul class="overflow">
 			<li>
 				<a href="mailto:hanee.devel@gmail.com" class="internal">
-					<img src="https://img.shields.io/badge/Gmail-EA4335?style=for-the-badge&logo=Gmail&logoColor=white"> 
-				</a> &nbsp;
+					<img src="https://img.shields.io/badge/Gmail-EA4335?style=for-the-badge&logo=Gmail&logoColor=white" /> 
+				</a>
+			</li>
+			<li>
 				<a href="https://open.kakao.com/o/sQMdBXCf" class="internal">
-					<img src="https://img.shields.io/badge/KakaoTalk-FFCD00?style=for-the-badge&logoColor=black&logo=KakaoTalk"> 
+					<img src="https://img.shields.io/badge/KakaoTalk-FFCD00?style=for-the-badge&logoColor=black&logo=KakaoTalk" /> 
 				</a>
 			</li>
 		</ul>
